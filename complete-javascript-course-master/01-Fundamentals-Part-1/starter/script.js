6c2b047e8af7acb467e73da6d5f82550fb5e6e82
DeepthiMurabijji <deepthimurabijji@gmail.com>
2022-05-31 18:28:10 +0530

    js

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let js = "amazing";
+// if (js === 'amazing') alert("javascript");
+
+console.log(40 + 8 + 23 - 10);  
+console.log("jonas");
+
+let firstname = "jonas";
+ 
+// 
+//#we cannot use numbers in decalration of variabls like 
+// they cannot star with numbers
+
+
+// FIXME:  let 3years =3;
+//#region

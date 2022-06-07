6fdfbfe32de6078e395ed24cd1536dd329b6b53b
DeepthiMurabijji <deepthimurabijji@gmail.com>
2022-06-07 18:57:02 +0530

    js-2

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive 🚕") 
+
+//   without [use strict]   and with error hasDriverLicense
+// the output :  _______
+
+// same without [use strict] but correct variable name hasDriversLicense
+// the output : I can drive 🚕
+
+// with [use strict] and with error hasDriverLicense
+// the output : it show an error  
+// Uncaught ReferenceError: hasDriverLicense is not defined at script.js:5:32
+
+// with [use strict] and no error hasDriverLicense
+// the output : I can drive 🚕
+
+
+// const interface = 'Audio';
+// const private = 534;
+// output: it shows an error message 
+// Uncaught SyntaxError: Unexpected strict mode reserved word (at script.js:21:7)
+
+
+
+// functions:
+
+function logger()
+{
+    console.log("My name is Jonas");
+}
+
+logger();         // caling  / running / invoking function 
+logger();           // you can call any number of times 
+
+
+function fruitProcessor(apples , oranges)
+{
+    //console.log(apples , oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(5, 3);     // output : 5 3 
+const appleJuice = fruitProcessor(5 ,3);
+console.log(appleJuice);           // output : juice with 5 apples and 3 oranges.
+
+console.log(fruitProcessor(5,0));  // output :  juice with 5 apples and 0 oranges.
+*/
+
+
+function calcAge1(birthYeah)
+{
+    const age = 2037 - birthYeah;
+    
+}
+
+
+
+

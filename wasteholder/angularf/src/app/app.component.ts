af7762431d57555124d9d6a5c1808610b40890ae
DeepthiMurabijji <deepthimurabijji@gmail.com>
2022-06-30 12:03:03 +0530

    few changes to navbar

--- wasteholder/angularf/src/app/app.component.ts
+++ wasteholder/angularf/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { TrashService } from './services/trash.service';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'angularf';
+
+  constructor(public trash:TrashService,public auth:AuthService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  logout(){
+    localStorage.clear()
+    sessionStorage.clear()
+    this.router.navigate([''])
+  }
 }

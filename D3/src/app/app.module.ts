d5bd669a87110b75256874eddc7d46f2230ea843
DeepthiMurabijji <deepthimurabijji@gmail.com>
2022-07-27 15:47:29 +0530

    piecharts

--- D3/src/app/app.module.ts
+++ D3/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarChartVerticalComponent } from './bar-chart-vertical/bar-chart-vertical.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartVerticalComponent
   ],
   imports: [
     BrowserModule,
